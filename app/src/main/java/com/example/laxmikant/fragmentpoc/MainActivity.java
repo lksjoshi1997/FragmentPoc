8883dc2d046a19f6754b3d340f63fa2cd030280a
lksjoshi1997 <lksjoshi1997@gmail.com>
2019-05-05 10:21:37 +0530

    Fragment added to main activity

--- app/src/main/java/com/example/laxmikant/fragmentpoc/MainActivity.java
+++ app/src/main/java/com/example/laxmikant/fragmentpoc/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.laxmikant.fragmentpoc;
 
+import android.app.Fragment;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    Button firstFragmentBtn;
+    Button secondFragmentBtn;
+    Fragment mainFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initVariable();
+        initListeners();
+        startWorking();
+    }
+
+    private void initVariable() {
+        View main = findViewById(R.id.main_activity);
+        firstFragmentBtn = main.findViewById(R.id.fragmentOne);
+        secondFragmentBtn = main.findViewById(R.id.fragmentTwo);
+    }
+
+    private void initListeners() {
+    }
+
+    private void startWorking() {
     }
 }
